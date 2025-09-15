68853bf284d8669799c683b50f19e63e9fbb1641
Daniel Broomhead <broomhead.dl@gmail.com>
2025-09-15 22:37:20 +0100

    unspin confetti, colour change

--- src/lib/components/Layout/OS_banner2.svelte
+++ src/lib/components/Layout/OS_banner2.svelte
@@ -515,10 +515,18 @@
       transform: scale(1) rotate(0deg);
       filter: hue-rotate(0deg);
     }
+    25% { 
+      transform: scale(1.3) /*rotate(90deg)*/;
+      filter: hue-rotate(90deg);
+    }
     50% { 
-      transform: scale(1.5) rotate(360deg);
-      filter: hue-rotate(45deg);
-    }   
+      transform: scale(1.1) /*rotate(180deg)*/;
+      filter: hue-rotate(180deg);
+    }
+    75% { 
+      transform: scale(1.4) /*rotate(270deg)*/;
+      filter: hue-rotate(270deg);
+    }
   }
   
   .animals {
