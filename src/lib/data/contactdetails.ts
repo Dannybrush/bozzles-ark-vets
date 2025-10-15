b00955a02a6285265af52aa4ad82ac583e89cf96
Daniel Broomhead <broomhead.dl@gmail.com>
2025-10-15 15:35:40 +0100

    feat(data): convert contactdetails.js → contactdetails.ts with types

---
+++ src/lib/data/contactdetails.ts
@@ -0,0 +1,32 @@
+export const contactData = [
+  {
+    Name: "Bozzles Ark Veterinary Practice",
+    Landline: "01455 710 796", 
+    EmergencyLandline: "01455 710 796",
+    Email: "Care@BozzlesArkVets.co.uk",
+    CompanyNumber: "#16434819",
+    gmaps: 'https://maps.google.com/?q=39+Jersey+Way+Barwell+LE9+8HR',
+    google: 'https://share.google/Hf6KzhfneVcj0jTrx',
+    facebook: 'https://www.facebook.com/BozzlesArkVets',
+    instagram: 'https://www.instagram.com/bozzlesarkvets/',
+    address: { line1: "39 Jersey Way", line2: "Barwell", city: "Leicestershire", postcode: "LE9 8HR"}, 
+    website: "https://www.BozzlesArkVets.co.uk",
+    linkedin: 'https://www.linkedin.com/company/bozzles-ark-vets' // Doesnt exist yet
+
+
+];
+
+
+ export const openingHours = [
+    { day: 'Monday', hours: '9:00am - 7:00pm' },
+    { day: 'Tuesday', hours: '9:00am - 7:00pm' },
+    { day: 'Wednesday', hours: '9:00am - 7:00pm' },
+    { day: 'Thursday', hours: '9:00am - 7:00pm' },
+    { day: 'Friday', hours: '9:00am - 7:00pm' },
+    { day: 'Saturday', hours: '10:00am - 5:00pm' },
+    { day: 'Sunday', hours: '10:00am - 2:00pm)' },
+    { day: 'Mon-Fri', hours: '9:00am - 7:00pm' },
+    { day: 'Bank Holidays', hours: 'Emergency Only' },
+    { day: 'Emergency Services', hours: '24/7 Emergency Service Available' }
+
+  ];
