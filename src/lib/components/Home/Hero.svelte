2f456d9baf2afe56f0a76c762b1e670ea171a3ac
Daniel Broomhead <broomhead.dl@gmail.com>
2026-01-04 18:19:35 +0000

    change hero buttons

--- src/lib/components/Home/Hero.svelte
+++ src/lib/components/Home/Hero.svelte
@@ -15,7 +15,7 @@
         <p>Compassionate care for all creatures great and small</p>
         
         <div class="hero-buttons">
-          <a href=#PreRegistration class="btn btn-primary"> Pre-Register Now</a>
+          <a href=#PreRegistration class="btn btn-primary"> Register Now</a>
           <a href=#Services class="btn btn-secondary">Our Services</a>
         </div>
       </div>
